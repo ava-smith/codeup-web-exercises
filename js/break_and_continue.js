c811bf3ddbda288ada53dd9a5313e84e6cadcfcf
Ava Smith <ava013005@gmail.com>
2023-06-06 11:07:14 -0500

    completed break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,19 @@
-prompt('Enter an odd number between 1 and 50');
+"use strict";
+
+// let userResponse;
+//
+// while(true) {
+//     userResponse = prompt('Enter an odd number between 1 and 50');
+//     userResponse = parseInt(userResponse);
+//     if (userResponse % 2 !== 0 && userResponse >= 1 && userResponse <= 50) {
+//         console.log('Number to skip is: ' + userResponse);
+//         break;
+//     }
+// }
+//     for (let i = 1; i <= 50; i += 2) {
+//         if (i === userResponse) {
+//             console.log(`Yikes! Skipping number: ${userResponse}`);
+//             continue;
+//         }
+//         console.log(`Here is an odd number: ${i}`);
+// }
