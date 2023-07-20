bf604820271ce9c6eb4be351b0fc9fbbb57970be
Ava Smith <ava013005@gmail.com>
2023-07-20 14:17:31 -0500

    made a new exercise

--- js/map-filter-reduce-lesson.js
+++ js/map-filter-reduce-lesson.js
@@ -12,7 +12,7 @@
             return response.json();
         })
         .then(houses => {
-            console.log(houses);
+            // console.log(houses);
             // working with the houses array
             // FILTER METHOD ============================
             let housesTwoBedsPlus = houses.filter(house => {
@@ -21,7 +21,7 @@
                 const numberOfBaths = parseFloat(house.baths);
                 return numberOfBeds >= 4 && numberOfBaths >= 5;
             });
-            console.log(housesTwoBedsPlus);
+            // console.log(housesTwoBedsPlus);
 
             // MAP METHOD ================================
             // when you want to transform each element in an array
@@ -53,12 +53,66 @@
                 `;
                 return card;
             })
-            console.log(houseNodes);
+            // console.log(houseNodes);
             for(let house of houseNodes) {
                 document.querySelector('.container').appendChild(house);
             }
+            // REDUCE METHOD ========================
+            // find the most expensive house
+            // reduce method takes an anonymous function as its first argument, second is the initial value.
+            const largestPrice = houses.reduce(/* function parameter */ (accumulator, house, index) => {
+                let price = parseFloat(house.price);
+                if (price > accumulator) {
+                    accumulator = price;
+                }
+                return accumulator;
+            },
+            /* initial value for the accumulator */ 0);
+            console.log("most expensive =>", largestPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
 
+            // find the cheapest house
+            const cheapestPrice = houses.reduce(/* function parameter */ (accumulator, house, index) => {
+                    let price = parseFloat(house.price);
+                    if (price < accumulator) {
+                        accumulator = price;
+                    }
+                    return accumulator;
+                },
+                /* initial value for the accumulator */ parseFloat(houses[0].price));
+            console.log("least expensive =>",cheapestPrice.toLocaleString('en-US', {style: 'currency', currency: 'USD'}))
 
+            //find all the prices
+            const allPrices = houses.reduce(/* function parameter */ (accumulator, house, index) => {
+                    let price = parseFloat(house.price);
+                    accumulator += price;
+                    return accumulator;
+                },
+                /* initial value for the accumulator */ parseFloat(houses[0].price));
+            console.log("total price =>", allPrices.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
+
+            // How many houses were for sale in 2022
+            // what was the most expensive house
+            // what was the cheapest house
+            // what was the total market value of all houses for sale?
+
+            const dashboardObject = houses.reduce((accumulator, house, index) => {
+                accumulator.totalHouses++;
+                let price = parseFloat(house.price);
+                if (price > accumulator.mostExpensive) {
+                    accumulator.mostExpensive = price;
+                }
+                if (price < accumulator.cheapestHouse) {
+                    accumulator.cheapestHouse = price;
+                }
+                accumulator.totalMarketValue += price;
+                return accumulator;
+            }, {
+                totalHouses: 0,
+                mostExpensive: 0,
+                cheapestHouse: parseFloat(houses[0].price),
+                totalMarketValue: 0
+            });
+            console.log(dashboardObject);
         });
 
 }) ();
