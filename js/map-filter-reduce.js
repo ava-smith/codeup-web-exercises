b2284ff42aaddc364d8bd766d9823e43b62ee11a
Ava Smith <ava013005@gmail.com>
2023-07-21 09:45:36 -0500

    completed walkthrough

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,7 +41,7 @@ const users = [
 // TODO:
 //  Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 
-    let filterLanguages = users.filter(user => {
+    const filterLanguages = users.filter(user => {
        const userLanguages = user.languages;
        return userLanguages.length >= 3;
     });
@@ -50,19 +50,18 @@ const users = [
 // TODO:
 //  Use .map to create an array of strings where each element is a user's email address.
 
-    let mapEmails = users.map(user => {
-        const userEmails = user.email;
-        const emailString = `${userEmails}`;
-        return emailString;
-    });
+    const mapEmails = users.map(user => user.email);
     console.log(mapEmails);
 
 // TODO:
 //  Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
 
     const totalExperience = users.reduce((accumulator, user, index) => {
-        let experience = user.yearsOfExperience;
-        accumulator += experience / users.length;
+        let experience = parseFloat(user.yearsOfExperience);
+        accumulator += experience;
+        if (users.length-1 === index) {
+            accumulator = accumulator / users.length;
+        }
         return accumulator;
     }, 0);
 
@@ -73,7 +72,8 @@ const users = [
 
     const longestEmail = users.reduce((accumulator, user, index) => {
         let emailLengths = user.email.length;
-        if (emailLengths > accumulator.length) {
+        let accLength = accumulator.length;
+        if (emailLengths > accLength) {
             accumulator = user.email;
         }
         return accumulator;
@@ -85,9 +85,15 @@ const users = [
 //  Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
     const listOfNames = users.reduce((accumulator, user, index) => {
-        let comma = accumulator.length ? ", " : "";
-        return accumulator + comma + user.name;
-    }, "");
+        if (index === (users.length - 2)) {
+            accumulator += `${user.name} `;
+        } else if (index === (users.length - 1)) {
+            accumulator += `and ${user.name}.`;
+        } else {
+            accumulator += `${user.name}, `;
+        }
+        return accumulator;
+    }, "Your instructors are: ");
 
     console.log(listOfNames);
 
